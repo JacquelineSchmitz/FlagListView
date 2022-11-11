35e831c06228575f9aff8633c92329bddd7311c8
jacquelineschmitz <jacquelineschmitz81@gmail.com>
2022-11-11 15:18:55 +0100

    Initial Commit

---
+++ FlagListView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagListView
+//
+//  Created by Jacqueline Schmitz on 11.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
